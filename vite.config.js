0c24f281a71f597c5ef9b9ac679efd33a01bf912
ouienne <nathanarreza@gmail.com>
2025-08-25 08:53:41 +0800

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
       tailwindcss(),
-
   ],
+    // eslint-disable-next-line no-undef
+  base: process.env.VITE_BASE_PATH || "/greekmythologygame",
 })
